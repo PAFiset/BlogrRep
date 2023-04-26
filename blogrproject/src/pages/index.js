adcd0f936edc0d3933852efc3b6990f7b24914b5
PAFiset <jmpaof45@gmail.com>
2023-04-26 16:48:21 -0400

    hero banner with hover and responsive

--- blogrproject/src/pages/index.js
+++ blogrproject/src/pages/index.js
@@ -9,8 +9,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
-        allo!!!!
+      <main >
+        <div className="herobanner">
+          <img src="./images/blogr/bg-pattern-intro-desktop.svg" alt="Backgrounds pattern" className="desktop_pattern"/>
+          <img src="./images/blogr/bg-pattern-intro-mobile.svg" alt="Backgrounds pattern" className="mobile_pattern"/>
+
+          <h1>
+            A modern publishing platform
+          </h1>
+          <h2>
+            Grow your audience and build your online brand
+          </h2>
+          <div className="options">
+            <button>Start For Free</button>
+            <button>Learn More</button>
+          </div>
+
+        </div>
       </main>
     </>
   )
